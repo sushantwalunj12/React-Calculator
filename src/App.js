3fb7a40bd57468f25fcbd920d19dd7a55510f204
Sushant <sushantwalunj12@gmail.com>
2023-05-13 22:15:22 +0530

    R C Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './index.css';
 
 function App() {
+
+  let [input1, setInput1] = useState(""); 
+  let [input2, setInput2] = useState("");
+  let [result, setResult] = useState("");
+  let [message, setMessage] = useState("");
+  let [error, setError] = useState("");
+  
+  const validate = () => {
+    if (input1 === "") {
+      setError("Error : Num1 cannot be empty");
+      return false;
+    } else if (input2 === "") {
+      setError("Error : Num2 cannot be empty");
+      return false;
+    } else if (isNaN(input1) || isNaN(input2)) {
+      setError("Error : Enter valid input");
+      return false;
+    }
+    return true;
+  }
+
+  const calculate = (e) => {
+    setResult("");
+    setError("");
+    setMessage("");
+    if(!validate()) {
+      return;
+    }
+
+    let operator = e.target.innerText;
+    let answer;
+
+    if(operator === "+") {
+      answer = Number(input1) + Number(input2);
+    } else if(operator === "-") {
+      answer = input1 - input2;
+    } else if(operator === "*") {
+      answer = input1 * input2;
+    } else if(operator === "/") {
+      answer = input1 / input2;
+    } 
+    setResult(answer);
+    setMessage("Success : Your result is shown above!");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>React Calculator</h1>
+        <input type="text" placeholder='Num 1' id='input1' onChange={(e) => setInput1(e.target.value.trim())}/>
+        <input type="text" placeholder='Num 2' id='input2' onChange={(e) => setInput2(e.target.value.trim())}/>
+      <div className='keypad'>
+        <button onClick={calculate}>+</button>
+        <button onClick={calculate}>-</button>
+        <button onClick={calculate}>*</button>
+        <button onClick={calculate}>/</button>
+      </div>
+
+      <div className='result'>{(result === "") ? "": "Result = " + (result)}</div>
+      <div className='error'>{error}</div>
+      <div className='message'>{message}</div>
     </div>
   );
 }
